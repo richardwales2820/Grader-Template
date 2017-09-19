bc3681813f3b30ae2110461f3844a0fc07bdddc7
Richie Wales <richard_wales@knights.ucf.edu>
2017-09-19 00:20:04 -0400

    Added base models to database

--- speedgrader/grader/models.py
+++ speedgrader/grader/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Assignment(models.Model):
+    name = models.CharField(max_length=128)
+    total_score = models.IntegerField()
+    instructions = models.CharField(max_length=1024)
+
+class Feedback(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    comment = models.CharField(max_length=256)
+    
